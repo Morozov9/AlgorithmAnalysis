53f45a5ecd315be86436b276812bb1a3acf61d56
Илья <irotgan777@mail.ru>
2026-09-12 16:54:00 +0500

    сделал buble sort

--- Models/Algorithms/BubbleSortAlgorithm.cs
+++ Models/Algorithms/BubbleSortAlgorithm.cs
@@ -17,8 +17,19 @@ public class BubbleSortAlgorithm : VectorAlgorithm
 
     public override void Execute()
     {
-        // TODO: Реализовать алгоритм сортировки пузырьком
-        // Работать с массивом _workingData
-        throw new NotImplementedException("Реализуйте сортировку пузырьком");
+        int n = _workingData.Length;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - 1 - i; j++)
+            {
+                if (_workingData[j] > _workingData[j + 1])
+                {
+                    int temp = _workingData[j];
+                    _workingData[j] = _workingData[j + 1];
+                    _workingData[j + 1] = temp;
+                }
+            }
+        }
     }
 }
